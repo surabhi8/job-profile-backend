9a0b8db726c9b76b4d40bbe329d885c3a993cf72
surabhi8 <surabhigupta2442@gmail.com>
2019-08-14 11:14:37 +0530

    attempt 2

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 const routes = require('./routes/index');
-
 server.connection({
   port: process.env.PORT || 4000,
   host: process.env.NODE_HOST || '0.0.0.0',
