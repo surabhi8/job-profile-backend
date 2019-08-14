5475a2ba6bfa69a026178466d8d5bcb278c6d130
surabhi8 <surabhigupta2442@gmail.com>
2019-08-14 13:31:02 +0530

    update config

--- models/index.js
+++ models/index.js
@@ -9,11 +9,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres'
-  });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
