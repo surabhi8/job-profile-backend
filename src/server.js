252fe61792b0093a7e28559d12e28a5820932cf3
surabhi8 <surabhigupta2442@gmail.com>
2019-08-16 12:28:54 +0530

    cors enabled

--- src/server.js
+++ src/server.js
@@ -7,16 +7,8 @@ server.connection({
   port: process.env.PORT || 4000,
   host: process.env.NODE_HOST || '0.0.0.0',
   routes: {
-    cors: {
-        origin: ['*'], // an array of origins or 'ignore'
-        headers: ['Authorization'], // an array of strings - 'Access-Control-Allow-Headers' 
-        exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
-        additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
-        maxAge: 60,
-        credentials: true // boolean - 'Access-Control-Allow-Credentials'
-    }
-  }   
-});
+    cors: true
+}});
 server.route(routes);
 if (!module.parent) {
   server.start((err) => {
